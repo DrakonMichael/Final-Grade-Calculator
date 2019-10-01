2b2823d51f09fa183387d995efcdfee5e3ff224f
Michael Karpov <mkarpov@bsd220.org>
2019-10-01 10:18:12 -0500

    Initial Commit

---
+++ Final Grade Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Final Grade Calculator
+//
+//  Created by Michael Karpov on 10/1/19.
+//  Copyright © 2019 mkarpov@bhs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
